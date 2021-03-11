9c136810d866f949180dd20c1dac403dabad2ed4
tatsuya.kb <bis.kobayashi@gmail.com>
2021-03-11 13:10:12 +0900

    勤務場所（WorkPlaces）テーブル作成
    Model、migrationファイルの作成

---
+++ app/Models/WorkPlace.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\DB;
+
+class WorkPlace extends Model
+{
+    use SoftDeletes;
+
+    public function scopeIdAt($query, $id) {
+        return $query->where('id', $id);
+    }
+
+    public function scopeNameAt($query, $name) {
+        return $query->where('name', $name);
+    }
+}
---
+++ database/migrations/2021_03_11_024306_create_workplaces_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWorkplacesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('workplaces', function (Blueprint $table) {
+            $table->id()->unsigned()->comment('（主キー）ID');
+            $table->string('name')->comment('勤務場所');
+            $table->timestamps();
+            $table->softDeletes($column = 'deleted_at', $precision = 0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('workplaces');
+    }
+}
