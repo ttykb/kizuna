30252443dcdc11af4bcd4a20a486119d4af11571
tatsuya.kb <bis.kobayashi@gmail.com>
2021-03-11 18:03:46 +0900

    リファクタリング対応
    勤務場所「WorkPlace」→「Workplace」への変更に伴う、ファイル名・記述内容修正

--- database/seeders/WorkplaceSeeder.php
+++ database/seeders/WorkplaceSeeder.php
@@ -3,8 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
-class WorkTypeSeeder extends Seeder
+class WorkplaceSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -13,6 +14,25 @@ class WorkTypeSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $i = 1;
+        $Workplaces = [
+            "絆",
+            "中川",
+            "セイショウ",
+            "シーテック",
+            "佐々木工業",
+            "風組",
+            "第一健康江島",
+            "匠",
+            "川重産業",
+            "東栄建設"
+        ];
+
+        foreach($Workplaces as $Workplace) {
+            DB::table('workplaces')->insert([
+                'name' => $Workplace,
+                'created_at' => date('Y-m-d H:i:s')
+            ]);
+        }
     }
 }
